6012a7ed127d549cad81dd9d3f4762f197ec9442
Quan Vuong <quan.a.vuong@gmail.com>
2018-07-06 10:58:51 -0400

    add render(): render Greetings component on the page with button

--- template/static/js/Greetings.jsx
+++ template/static/js/Greetings.jsx
@@ -1,14 +1,24 @@
 export default class Greetings extends React.Component {
     constructor(props) {
         super(props);
-        this.setState = {greetings: "Welcome " + this.props.name};
+        this.setState = {greeting: "Welcome " + this.props.name};
 
         this.getPythonGreetings = this.getPythonGreetings.bind(this); // makes "this" work in the callback
     }
 
     personalize() {
         this.setState({
-            greetings: greetings + " " + this.props.name + "!"
+            greeting: greeting + " " + this.props.name + "!"
         });
+    }
+
+    render() {
+        return (
+            <h1> { this.state.greeting } </h1>
+            <hr/>
+            <Button bsSize="large" bsStyle="danger" onClick={this.getPythonGreetings} >
+                More Greetings!
+            </Button>
+        )
     }
 }
