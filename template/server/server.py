d0e4c1af9192854ce9b7f41e408105558b2e3ebd
Quan Vuong <quan.a.vuong@gmail.com>
2018-08-06 17:55:58 -0400

    update header image path

--- template/server/server.py
+++ template/server/server.py
@@ -13,4 +13,4 @@ def get_greetings():
     return random.choice(greetings)
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=3000, debug=True)
+    app.run(host="127.0.0.1", port=3000, debug=True)   
